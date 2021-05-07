499acdee5cab3cafd21b19dcd7d936edf6f3f1e7
kldyckman <kldyckman@gmail.com>
2021-05-07 15:59:05 -0400

    Update AppointmentsPage.js

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = (props) => {
   /*
   Define state variables for 
   appointment info
   */
+  const appointments = props.appointments;
+  const contacts = props.contacts;
+  const addAppointment = props.addAppointment;
+
+  const [title, setTitle] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
+    addAppointment(title, contact, date, time);
+    setTitle('');
+    setContact('');
+    setDate('');
+    setTime('');
    
   };
 
@@ -18,10 +34,23 @@ export const AppointmentsPage = () => {
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          title={title}
+          contact={contact}
+          date={date}
+          time={time}
+          setTitle={setTitle}
+          setContact={setContact}
+          setDate={setDate}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+         />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList
+        objArr={appointments} />
       </section>
     </div>
   );
