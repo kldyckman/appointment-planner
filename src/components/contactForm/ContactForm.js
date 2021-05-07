b82f141ee670eddb45d6e45f5b75d03a1f6c042f
kldyckman <kldyckman@gmail.com>
2021-05-07 15:05:12 -0400

    Update ContactForm.js

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,29 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={({target}) => {setName(target.value)}}
+        required
+       />
+      <input
+        type="tel"
+        value={phone}
+        pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+        onChange={({target}) => {setPhone(target.value)}}
+        required
+       />
+      <input
+        type="email"
+        value={email}
+        onChange={({target}) => {setEmail(target.value)}}
+        required
+       />
+      <input
+        type="submit"
+       />
+    </form>
   );
 };
